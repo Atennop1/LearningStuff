7170c3e3b56a353efb5d642d246d1cf1dbdc9ebb
AtennopVer <anton7185569@gmail.com>
2023-01-09 19:13:43 +0300

    Added disposable

--- Assets/Source/UniRX/ReactivePropertyExample/WalletView.cs
+++ Assets/Source/UniRX/ReactivePropertyExample/WalletView.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,19 +9,30 @@ namespace LearningStuff.UniRX.ReactivePropertyExample
     public class WalletView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _moneyText;
+        
+        [Space]
         [SerializeField] private Button _addButton;
         [SerializeField] private Button _decreaseButton;
+        [SerializeField] private Button _disposeButton;
+
+        private readonly CompositeDisposable _disposable = new();
 
         private void Awake()
         {
             var wallet = new Wallet();
-            wallet.MoneyCount.Subscribe(value => _moneyText.text = value.ToString());
+            
+            wallet.MoneyCount
+                .Subscribe(value => _moneyText.text = value.ToString())
+                .AddTo(_disposable);
+            
+            _disposeButton.OnClickAsObservable()
+                .Subscribe(_ => _disposable.Clear()).AddTo(_disposable);
 
             _addButton.OnClickAsObservable()
-                .Subscribe(_ => wallet.Put(1));
+                .Subscribe(_ => wallet.Put(1)).AddTo(_disposable);
 
             _decreaseButton.OnClickAsObservable()
-                .Subscribe(_ => wallet.Take(1));
+                .Subscribe(_ => wallet.Take(1)).AddTo(_disposable);
         }
     }
 }
