c555faf422345060ad1f8d5a5bc82910f4355f9d
AtennopVer <anton7185569@gmail.com>
2023-01-22 17:16:39 +0300

    Added players count in room display

--- Assets/Source/Photon/Callbacks/JoinRoomCallback.cs
+++ Assets/Source/Photon/Callbacks/JoinRoomCallback.cs
@@ -1,4 +1,5 @@
 ﻿using LearningStuff.Photon.Chat;
+using LearningStuff.Photon.Rooms;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
@@ -7,13 +8,25 @@ namespace LearningStuff.Photon.Callbacks
 {
     public class JoinRoomCallback : MonoBehaviourPunCallbacks
     {
-       [SerializeField] private MessageWriter _messageWriter;
+        [SerializeField] private PhotonView _photonView;
+        [SerializeField] private MessageWriter _messageWriter;
+       [SerializeField] private RoomPlayersView _roomPlayersView;
        
        public override void OnPlayerEnteredRoom(Player newPlayer)
-           => WriteMessage($"{newPlayer.NickName} has joined the room");
+       {
+           _photonView.RPC("DisplayPlayers", RpcTarget.All);
+           WriteMessage($"{newPlayer.NickName} has joined the room");
+       }
 
-       public override void OnJoinedRoom() 
-           => WriteMessage($"{PhotonNetwork.NickName} has joined the room");
+       public override void OnJoinedRoom()
+       {
+           _photonView.RPC("DisplayPlayers", RpcTarget.All);
+           WriteMessage($"{PhotonNetwork.NickName} has joined the room");
+       }
+
+       [PunRPC]
+       private void DisplayPlayers()
+           => _roomPlayersView.Display();
 
        private void WriteMessage(string message)
            => _messageWriter.WriteMessage(new Message("SYSTEM", message));
---
+++ Assets/Source/Photon/Rooms/RoomPlayersView.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+namespace LearningStuff.Photon.Rooms
+{
+    public class RoomPlayersView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _playersText;
+
+        public void Display()
+        {
+            Debug.Log("Displayed");
+            _playersText.text =
+                $"Players: {PhotonNetwork.CurrentRoom.PlayerCount}/{PhotonNetwork.CurrentRoom.MaxPlayers}";
+        }
+    }
+}
