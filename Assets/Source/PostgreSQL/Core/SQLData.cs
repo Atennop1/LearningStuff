82f3722fc91bc95c35a4c75e8766feb76f12bc90
AtennopVer <anton7185569@gmail.com>
2023-05-10 13:50:32 +0300

    More isolation

--- Assets/Source/PostgreSQL/Core/SQLData.cs
+++ Assets/Source/PostgreSQL/Core/SQLData.cs
@@ -11,6 +11,9 @@ namespace LearningStuff.PostgreSQL.Core
         {
             Name = name ?? throw new ArgumentException("Name can't be null");
             Value = value ?? throw new ArgumentNullException(nameof(value));
+
+            if (Value is string valueString && (string.IsNullOrEmpty(valueString) || valueString.IndexOfAny("&^\"\'@#$&|".ToCharArray()) != -1))
+                throw new ArgumentException("Value contains forbidden symbols");
         }
     }
 }
